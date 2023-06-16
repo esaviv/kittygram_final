41d65a7389c86abce54acb417e1dec1623e95aa5
Екатерина Савченко <KattySavchenko1@yandex.ru>
2023-06-16 14:00:12 +0200

    deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,11 +2,15 @@ import os
 from distutils.util import strtobool
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = strtobool(os.environ.get('DEBUG', 'false'))
+DEBUG = bool(strtobool(os.environ.get('DEBUG', 'false')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
